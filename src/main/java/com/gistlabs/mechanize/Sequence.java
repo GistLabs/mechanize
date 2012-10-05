b04b94c4794cec67e1dc2f9d7c8f5f40e8aaa4d0
MartinKersten <Martin.Kersten.mk@gmail.com>
2012-10-05 15:33:47 +0200

    Issue #7 Added Sequence interface

---
+++ src/main/java/com/gistlabs/mechanize/Sequence.java
@@ -0,0 +1,12 @@
+package com.gistlabs.mechanize;
+
+/**
+ * Describes a sequence of requests.
+ * 
+ * @author Martin Kersten <Martin.Kersten.mk@gmail.com>
+ */
+public interface Sequence {
+
+	public void run(MechanizeAgent agent);
+}
+ 
