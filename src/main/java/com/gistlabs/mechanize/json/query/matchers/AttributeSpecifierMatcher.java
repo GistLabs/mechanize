aa2ac3a1427d8e97c8c57d1641cbff8aa3e0fae2
John Heintz <john@gistlabs.com>
2012-10-10 09:06:16 -0500

    Abstracted attribute specifier into an abstract/generic class with an small implementation that ties to json Element. This worked just like I wanted

--- src/main/java/com/gistlabs/mechanize/json/query/matchers/AttributeSpecifierMatcher.java
+++ src/main/java/com/gistlabs/mechanize/json/query/matchers/AttributeSpecifierMatcher.java
@@ -5,92 +5,22 @@ import java.util.Collection;
 import java.util.List;
 
 import se.fishtank.css.selectors.specifier.AttributeSpecifier;
-import se.fishtank.css.util.Assert;
 
 import com.gistlabs.mechanize.json.Element;
 import com.gistlabs.mechanize.json.query.Matcher;
 
-public class AttributeSpecifierMatcher implements Matcher<Element> {
+public class AttributeSpecifierMatcher extends AbstractAttributeSpecifierMatcher<Element> implements Matcher<Element> {
 	
-    /** The attribute specifier to check against. */
-    private final AttributeSpecifier specifier;
-   
     /**
      * Create a new instance.
      * 
      * @param specifier The attribute specifier to check against. 
      */
     public AttributeSpecifierMatcher(AttributeSpecifier specifier) {
-        Assert.notNull(specifier, "specifier is null!");
-        this.specifier = specifier;
+    	super(specifier);
     }
     
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Collection<Element> match(Collection<Element> nodes, Element root) {
-        Assert.notNull(nodes, "nodes is null!");
-        Collection<Element> result = new ArrayList<Element>();
-        for (Element node : nodes) {
-        	// It just have to be present.
-            String name = specifier.getName();
-			if (specifier.getValue() == null && hasAttribute(node, name)) {
-                result.add(node);
-                continue;
-            }
-            
-            Collection<Element> attributes = getAttributes(node);
-            for (Element element : attributes) {
-            	String value = getValue(element);
-                String spec = specifier.getValue();
-                switch (specifier.getMatch()) {
-                case EXACT:
-                    if (value.equals(spec)) {
-                        result.add(node);
-                    }
-                    
-                    break;
-                case HYPHEN:
-                    if (value.equals(spec) || value.startsWith(spec + '-')) {
-                        result.add(node);
-                    }
-                    
-                    break;
-                case PREFIX:
-                    if (value.startsWith(spec)) {
-                        result.add(node);
-                    }
-                    
-                    break;
-                case SUFFIX:
-                    if (value.endsWith(spec)) {
-                        result.add(node);
-                    }
-                    
-                    break;
-                case CONTAINS:
-                    if (value.contains(spec)) {
-                        result.add(node);
-                    }
-                    
-                    break;
-                case LIST:
-                    for (String v : value.split("\\s+")) {
-                        if (v.equals(spec)) {
-                            result.add(node);
-                        }
-                    }
-                    
-                    break;
-                }
-            }
-        }
-        
-        return result;
-    }
-
-	protected String getValue(Element element) {
+    protected String getValue(Element element) {
 		return element.getContent();
 	}
 	
