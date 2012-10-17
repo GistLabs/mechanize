87b70cc284e57fd3e93e9249ae96d266c1b41511
John Heintz <john@gistlabs.com>
2012-10-17 13:23:28 -0500

    Better Node documentation

--- src/main/java/com/gistlabs/mechanize/document/node/Node.java
+++ src/main/java/com/gistlabs/mechanize/document/node/Node.java
@@ -9,27 +9,38 @@ import com.gistlabs.mechanize.document.query.AbstractQuery;
  * 
  * <p>To support additional interpreted attributes special attribute names can be used following the notation ${attributeName}.
  *    Refer to the current node implementation for supported special attributes (e.g <code>HtmlElement.getAttribute("${text}")</code>)</p>
- *  
+ * 
  * @author Martin Kersten <Martin.Kersten.mk@gmail.com>
  */
 public interface Node {
 
 	/** Returns the name of the node or null if none (similar to getAttribute(SpecialAttribute.SPECIAL_ATTRIBUTE_NAME)). */
 	String getName();
-	
+
 	/** Returns the name of the node or null if none (similar to getAttribute(SpecialAttribute.SPECIAL_ATTRIBUTE_VALUE)). */
 	String getValue();
-	
+
 	/** Returns the first child element matching the query by performing a deep first left right search. */
 	Node get(AbstractQuery<?> query);
 
 	/** Returns all child elements matching the query by performing a deep first left right search. */
 	List<? extends Node> getAll(AbstractQuery<?> query);
 
-	/** Returns the only child node matching the CSS Selector search query */
+	/**
+	 * Returns the only child node matching the CSS Selector search query
+	 * See http://www.w3.org/TR/css3-selectors/ for syntax
+	 * @param query
+	 * @return matching single Node, null, or exception if multiple
+	 */
 	public <T extends Node> T find(String query);
 
-	/** Returns all descendant nodes matching the CSS Selector search query */
+	/**
+	 * Return the matching nodes for the CSS Selector search query
+	 * See http://www.w3.org/TR/css3-selectors/ for syntax
+
+	 * @param query
+	 * @return a non-null list of matching nodes, may be empty
+	 */
 	public List<? extends Node> findAll(String query);
 
 	/** Returns the child elements. */
@@ -37,18 +48,18 @@ public interface Node {
 
 	/** Returns the child elements. */
 	List<? extends Node> getChildren(String... names);
-	
+
 	/** Returns the parent node or null in case of a root node. */
 	Node getParent();
 
 	/** Returns true if the attribute is set and has a value of if a special attribute (${name}) is supported. */
 	boolean hasAttribute(String attributeKey);
 
-	/** Returns the value of the attribute. */ 
+	/** Returns the value of the attribute. */
 	String getAttribute(String attributeKey);
-	
+
 	/** Returns all attribute names being present including any supported special attribute. */
 	List<String> getAttributeNames();
-	
+
 	void visit(NodeVisitor visitor);
 }
