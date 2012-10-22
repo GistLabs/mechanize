668eb2e025c0f339292ce25c867f2094a3ebdee1
John Heintz <john@gistlabs.com>
2012-10-22 10:44:07 -0500

    #57 enable wikipedia image checks with > X style checking instead of exact byte count.

--- src/test/java/com/gistlabs/mechanize/integration/test/WikipediaSearchForAngelaMerkelAndDownloadingImagesIT.java
+++ src/test/java/com/gistlabs/mechanize/integration/test/WikipediaSearchForAngelaMerkelAndDownloadingImagesIT.java
@@ -8,9 +8,7 @@
 package com.gistlabs.mechanize.integration.test;
 
 import static com.gistlabs.mechanize.document.html.query.HtmlQueryBuilder.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -28,7 +26,7 @@ import com.gistlabs.mechanize.link.Links;
  * @author Martin Kersten<Martin.Kersten.mk@gmail.com>
  */
 public class WikipediaSearchForAngelaMerkelAndDownloadingImagesIT {
-	
+
 	@Test
 	public void testLoadWikipediaIndexPage() {
 		MechanizeAgent agent = new MechanizeAgent();
@@ -39,7 +37,7 @@ public class WikipediaSearchForAngelaMerkelAndDownloadingImagesIT {
 		assertTrue(links.size() > 10);
 		assertNotNull(links.get(byTitle(regEx(".*English.*"))));
 	}
-	
+
 	@Test
 	public void testClickingEnglishWikipediaVersionLink() {
 		MechanizeAgent agent = new MechanizeAgent();
@@ -53,27 +51,26 @@ public class WikipediaSearchForAngelaMerkelAndDownloadingImagesIT {
 		Resource englishPage = link.click();
 		assertEquals("Wikipedia, the free encyclopedia", englishPage.getTitle());
 	}
-	
+
 	@Test
 	public void testSearchingWikipediaForAngelaMerkelInGermanLanguageUtilizingSelectAndTextInput() {
 		MechanizeAgent agent = new MechanizeAgent();
 		Page page = agent.get("http://www.wikipedia.org");
 		Form form = page.forms().get(byClass("search-form"));
 		form.getSelect(byName("language")).getOption("de").select();
-		form.getSearch(byName("search")).set("Angela Merkel"); 
+		form.getSearch(byName("search")).set("Angela Merkel");
 		Resource response = form.getSubmitButton(byName("go")).submit();
 		assertTrue(response.getTitle().startsWith("Angela Merkel"));
 	}
-	
+
 	@Test
 	public void testDownloadWikipediaLogoImagesToBuffer() {
 		MechanizeAgent agent = new MechanizeAgent();
 		Page page = agent.get("http://www.wikipedia.org");
 		List<Image> images = page.images().getAll(byHtml(regEx(".*Wikipedia.*")));
 		assertEquals(2, images.size());
-		
-		// these are fragile... changed today
-//		assertEquals(2479, images.get(0).get().getLength());
-//		assertEquals(45283, images.get(1).get().getLength());
+
+		assertTrue(images.get(0).get().getLength() > 2000);
+		assertTrue(images.get(1).get().getLength() > 40000);
 	}
 }
