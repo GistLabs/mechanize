a47c54f6076691a5810d5cb9f819acc8ebc5f3b6
John Heintz <john@gistlabs.com>
2013-05-10 07:51:44 -0500

    #67 finally made CacheEntry a public interface

--- src/main/java/com/gistlabs/mechanize/cache/CacheEntry.java
+++ src/main/java/com/gistlabs/mechanize/cache/CacheEntry.java
@@ -3,41 +3,41 @@ package com.gistlabs.mechanize.cache;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 
-interface CacheEntry {
+public interface CacheEntry {
 
 	/**
 	 * Is this a cacheable response? Does it indicate either caching or conditional checks?
 	 */
-	public abstract boolean isCacheable();
+	public boolean isCacheable();
 
 	/**
 	 * Is this cache entry still within valid time checks? (Don't need to call the server...)
 	 *
 	 * @return
 	 */
-	public abstract boolean isValid();
+	public boolean isValid();
 
 	/**
 	 * The size of the entity content
 	 *
 	 * @return
 	 */
-	public abstract long byteCount();
+	public long byteCount();
 
 	/**
 	 * @return Get the cached response
 	 */
-	public abstract HttpResponse getResponse();
+	public HttpResponse getResponse();
 
-	public abstract CacheEntry updateCacheValues(final HttpResponse response);
+	public CacheEntry updateCacheValues(final HttpResponse response);
 
-	public abstract void prepareConditionalGet(final HttpUriRequest newRequest);
+	public void prepareConditionalGet(final HttpUriRequest newRequest);
 
 	/**
 	 * Return a response to a HEAD request (status line, headers, no entity body)
 	 * @return
 	 */
-	public abstract HttpResponse head();
+	public HttpResponse head();
 
 }
 
