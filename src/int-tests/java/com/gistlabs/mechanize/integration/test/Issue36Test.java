9871402f4121c6b543d2617b97d16c5c7c29a120
John Heintz <john@gistlabs.com>
2014-07-08 17:17:57 -0500

    Move this into integration tests

--- src/int-tests/java/com/gistlabs/mechanize/integration/test/Issue36Test.java
+++ src/int-tests/java/com/gistlabs/mechanize/integration/test/Issue36Test.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.gistlabs.mechanize.debug;
+package com.gistlabs.mechanize.integration.test;
 
 import java.io.IOException;
 import java.net.ProxySelector;
