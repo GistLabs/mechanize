46a745a994d1cd01aaf65cb74d38e2c1b4e57390
John Heintz <john@gistlabs.com>
2014-07-10 12:13:17 -0500

    Success, expansion of array works.

--- src/main/java/com/gistlabs/mechanize/document/json/node/JsonLink.java
+++ src/main/java/com/gistlabs/mechanize/document/json/node/JsonLink.java
@@ -9,6 +9,8 @@ package com.gistlabs.mechanize.document.json.node;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.damnhandy.uri.template.UriTemplate;
 
@@ -55,13 +57,33 @@ public class JsonLink {
 			String[] variables = template.getVariables();
 			if (variables.length>0) {
 				for (String var : variables) {
-					template.set(var, node.getAttribute(var));
+					template.set(var, lookupVar(var));
 				}				
 			}
 			
 			return template.expand();
 		} catch (Exception e) {
 			throw new RuntimeException(String.format("Problem processing %s", combined), e);
+		}
+	}
+
+	/**
+	 * Return an Object that is either String or List<String>
+	 * @param var maybe null if not found
+	 * @return
+	 */
+	protected Object lookupVar(String var) {
+		List<? extends JsonNode> children = node.getChildren(var);
+		if (children.size()>1) { // multiple with name
+			List<String> result = new ArrayList<String>();
+			for (JsonNode child : children) {
+				result.add(child.getValue());
+			}
+			return result;
+		} else if (children.size()==1) { // treat a single as an attribute
+			return children.get(0).getValue();
+		} else { // return null
+			return null;
 		}
 	}
 
